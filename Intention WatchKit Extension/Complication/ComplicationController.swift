9fa0faa9b95961574e13bdf10ac6b7087f7780e3
paulweichhart <paul.weichhart@me.com>
2020-10-24 23:55:45 +0200

    Finished Complication

--- Intention WatchKit Extension/Complication/ComplicationController.swift
+++ Intention WatchKit Extension/Complication/ComplicationController.swift
@@ -12,30 +12,17 @@ final class ComplicationController: NSObject, CLKComplicationDataSource {
     
     // MARK: - Complication Configuration
 
-    func getComplicationDescriptors(handler: @escaping ([CLKComplicationDescriptor]) -> Void) {
-        let descriptors = [
-            CLKComplicationDescriptor(identifier: "complication", displayName: "Intention", supportedFamilies: [.circularSmall, .graphicCircular])
-        ]
-        handler(descriptors)
-    }
-    
-//    func handleSharedComplicationDescriptors(_ complicationDescriptors: [CLKComplicationDescriptor]) {
-//        // Do any necessary work to support these newly shared complication descriptors
+//    func getComplicationDescriptors(handler: @escaping ([CLKComplicationDescriptor]) -> Void) {
+//        let descriptors = [
+//            CLKComplicationDescriptor(identifier: "complication", displayName: "Intention", supportedFamilies: [.circularSmall, .graphicCircular])
+//        ]
+//        handler(descriptors)
 //    }
 
-    // MARK: - Timeline Configuration
-    
-//    func getTimelineEndDate(for complication: CLKComplication, withHandler handler: @escaping (Date?) -> Void) {
-//        // Call the handler with the last entry date you can currently provide or nil if you can't support future timelines
-//        handler(Date().addingTimeInterval(24.0 * 60.0 * 60.0))
-//    }
-//
     func getPrivacyBehavior(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationPrivacyBehavior) -> Void) {
         // Call the handler with your desired behavior when the device is locked
         handler(.hideOnLockScreen)
     }
-
-    // MARK: - Timeline Population
     
     func getCurrentTimelineEntry(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTimelineEntry?) -> Void) {
         
@@ -57,55 +44,4 @@ final class ComplicationController: NSObject, CLKComplicationDataSource {
             handler(nil)
         }
     }
-    
-//    func getTimelineEntries(for complication: CLKComplication, after date: Date, limit: Int, withHandler handler: @escaping ([CLKComplicationTimelineEntry]?) -> Void) {
-//        // Call the handler with the timeline entries after the given date
-//        let fiveMinutes = 5.0 * 60.0
-//        let twentyFourHours = 24.0 * 60.0 * 60.0
-//
-//        // Create an array to hold the timeline entries.
-//        var entries = [CLKComplicationTimelineEntry]()
-//
-//        // Calculate the start and end dates.
-//        var current = date.addingTimeInterval(fiveMinutes)
-//        let endDate = date.addingTimeInterval(twentyFourHours)
-//
-//        // Create a timeline entry for every five minutes from the starting time.
-//        // Stop once you reach the limit or the end date.
-//        while (current.compare(endDate) == .orderedAscending) && (entries.count < limit) {
-//            entries.append(createTimelineEntry(forComplication: complication, date: current))
-//            current = current.addingTimeInterval(fiveMinutes)
-//        }
-//
-//        handler(entries)
-//    }
-//
-//    // MARK: - Sample Templates
-//
-//    func getLocalizableSampleTemplate(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTemplate?) -> Void) {
-//        // This method will be called once per supported complication, and the results will be cached
-//        handler(nil)
-//    }
-//
-//    private func createTimelineEntry(forComplication complication: CLKComplication, date: Date) -> CLKComplicationTimelineEntry {
-//        let template = createGraphicCircleTemplate(forDate: date)
-//
-//        // Use the template and date to create a timeline entry.
-//        return CLKComplicationTimelineEntry(date: date, complicationTemplate: template)
-//    }
-//
-//    private func createGraphicCircleTemplate(forDate date: Date) -> CLKComplicationTemplate {
-//        // Create the data providers.
-//        let minutes: Double
-//        switch Store.shared.state {
-//        case .available, .initial, .error:
-//            minutes = 0
-//        case let .mindfulMinutes(mindfulMinutes):
-//            minutes = mindfulMinutes
-//        }
-//
-//
-//        let activeMinutes = CLKSimpleTextProvider(text: "\(Int(minutes))")
-//        return CLKComplicationTemplateGraphicCircularClosedGaugeText(gaugeProvider: provider, centerTextProvider: activeMinutes)
-//    }
 }
--- Intention WatchKit Extension/IntentionApp.swift
+++ Intention WatchKit Extension/IntentionApp.swift
@@ -11,7 +11,7 @@ import WatchKit
 @main
 struct IntentionApp: App {
     
-    @Environment(\.scenePhase) private var scenePhase
+    @WKExtensionDelegateAdaptor(ExtensionDelegate.self) var delegate
     
     private let intention = Intention()
     
@@ -26,65 +26,6 @@ struct IntentionApp: App {
             .tabViewStyle(PageTabViewStyle())
             .environmentObject(intention)
         }
-//        }.onChange(of: scenePhase, perform: { newScenePhase in
-//            if newScenePhase == .background {
-//                let watchExtension = WKExtension.shared()
-//                let targetDate = Date().addingTimeInterval(15.0 * 60.0)
-//                watchExtension.scheduleBackgroundRefresh(withPreferredDate: targetDate, userInfo: nil) { error in
-//                    if let error = error {
-//                        print("*** An background refresh error occurred: \(error.localizedDescription) ***")
-//                        return
-//                    }
-//                    print("*** Background Task Completed Successfully! ***")
-//                }
-//            }
-//        })
         WKNotificationScene(controller: NotificationController.self, category: "myCategory")
     }
 }
-
-//class ExtensionDelegate: NSObject, WKExtensionDelegate {
-//    
-//    func applicationDidEnterBackground() {
-//        scheduleBackgroundRefreshTasks()
-//    }
-//
-//    func handle(_ backgroundTasks: Set<WKRefreshBackgroundTask>) {
-//        for task in backgroundTasks {
-//            
-//            switch task {
-//            case let backgroundTask as WKApplicationRefreshBackgroundTask:
-//                Store.shared.mindfulMinutes(completion: { [weak self] in
-//                    self?.scheduleBackgroundRefreshTasks()
-//                    backgroundTask.setTaskCompletedWithSnapshot(true)
-//                })
-//                
-//            case let snapshotTask as WKSnapshotRefreshBackgroundTask:
-//                snapshotTask.setTaskCompleted(restoredDefaultState: true, estimatedSnapshotExpiration: Date.distantFuture, userInfo: nil)
-//            case let connectivityTask as WKWatchConnectivityRefreshBackgroundTask:
-//                connectivityTask.setTaskCompletedWithSnapshot(false)
-//            case let urlSessionTask as WKURLSessionRefreshBackgroundTask:
-//                urlSessionTask.setTaskCompletedWithSnapshot(false)
-//            case let relevantShortcutTask as WKRelevantShortcutRefreshBackgroundTask:
-//                relevantShortcutTask.setTaskCompletedWithSnapshot(false)
-//            case let intentDidRunTask as WKIntentDidRunRefreshBackgroundTask:
-//                intentDidRunTask.setTaskCompletedWithSnapshot(false)
-//            default:
-//                task.setTaskCompletedWithSnapshot(false)
-//            }
-//        }
-//    }
-//    
-//    private func scheduleBackgroundRefreshTasks() {
-//        
-//        let watchExtension = WKExtension.shared()
-//        let targetDate = Date().addingTimeInterval(15.0 * 60.0)
-//        watchExtension.scheduleBackgroundRefresh(withPreferredDate: targetDate, userInfo: nil) { (error) in
-//            if let error = error {
-//                print("*** An background refresh error occurred: \(error.localizedDescription) ***")
-//                return
-//            }
-//            print("*** Background Task Completed Successfully! ***")
-//        }
-//    }
-//}
